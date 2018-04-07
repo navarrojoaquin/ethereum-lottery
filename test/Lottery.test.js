2b000fc5a6a5d571f225ec58225c8a2642b140bb
Joaquín Navarro <joaquin.navarro.s@gmail.com>
2018-04-07 17:35:24 +0200

    Lottery end to end test

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -50,10 +50,24 @@ describe('Lottery', () => {
 
     it('only managers can call pickWinner', async () => {
         try {
-            await lottery.methods.pickWinner().send({ from: accounts[1], value: '200' });
+            await lottery.methods.pickWinner().send({ from: accounts[1] });
             assert(false);
         } catch (err) {
             assert(err);
         }
+    });
+
+    it('sends money to the winner and resets the players array', async () => {
+        await lottery.methods.enter().send({ from: accounts[0], value: web3.utils.toWei('2', 'ether') });
+
+        const initialBalance = await web3.eth.getBalance(accounts[0]);
+        await lottery.methods.pickWinner().send({ from: accounts[0] });
+        const finalBalance = await web3.eth.getBalance(accounts[0]);
+        const difference = finalBalance - initialBalance;
+
+        const players = await lottery.methods.getPlayers().call();
+
+        assert(difference > web3.utils.toWei('1.8', 'ether'));
+        assert.equal(0, players.length);
     });
 });
